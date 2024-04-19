95b0a9673255d1fd2f0ded094f2f292477961db6
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2024-04-19 19:15:03 +0300

    Fix cooldown manager racing

--- internal/cooldown.go
+++ internal/cooldown.go
@@ -2,10 +2,12 @@ package internal
 
 import (
 	"github.com/mrsobakin/pixelbattle/auth"
+	"sync"
 	"time"
 )
 
 type CooldownManager struct {
+	lock      sync.Mutex
 	cooldowns map[auth.UserId]time.Time
 	Cooldown  time.Duration
 }
@@ -18,6 +20,9 @@ func NewCooldownManager(cooldown time.Duration) *CooldownManager {
 }
 
 func (cm *CooldownManager) Attempt(id auth.UserId) (bool, *time.Duration) {
+	cm.lock.Lock()
+	defer cm.lock.Unlock()
+
 	last, has := cm.cooldowns[id]
 
 	if !has || time.Since(last) > cm.Cooldown {
